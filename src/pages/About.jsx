fea9bcf75a006aa740b6f3a4a0b2599baaed80e8
anandupg <162297648+anandupg@users.noreply.github.com>
2025-03-31 21:26:43 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,7 @@ function About() {
 
   const fetchProperties = async () => {
     try {
-      const response = await axios.get("http://localhost:3000/item/dis");
+      const response = await axios.get("https://react-backend-8s4z.onrender.com/item/dis");
       setProperties(response.data);
     } catch (error) {
       console.error("Error fetching properties:", error);
